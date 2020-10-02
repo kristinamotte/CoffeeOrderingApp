1d574936b26c338438ced50b77f8723e30c5fb58
Kristina <mottechristina@gmail.com>
2020-10-02 15:19:02 +0300

    create a project

---
+++ CoffeApp/CoffeApp/CoffeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeAppApp.swift
+//  CoffeApp
+//
+//  Created by Kristina Motte on 10/2/20.
+//
+
+import SwiftUI
+
+@main
+struct CoffeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeApp/CoffeApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoffeApp
+//
+//  Created by Kristina Motte on 10/2/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
