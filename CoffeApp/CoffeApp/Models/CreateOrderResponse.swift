9df998296cfe94bf426f4e6006dbb99884141b61
Kristina <mottechristina@gmail.com>
2020-10-15 22:01:14 +0300

    create coffee order

---
+++ CoffeApp/CoffeApp/Models/CreateOrderResponse.swift
@@ -0,0 +1,12 @@
+//
+//  CreateOrderResponse.swift
+//  CoffeApp
+//
+//  Created by Kristina Motte on 10/15/20.
+//
+
+import Foundation
+
+struct CreateOrderResponse: Decodable {
+    let success: Bool
+}
--- CoffeApp/CoffeApp/Services/CoffeeService.swift
+++ CoffeApp/CoffeApp/Services/CoffeeService.swift
@@ -8,6 +8,31 @@
 import Foundation
 
 class CoffeeService {
+    func createCoffeOrder(order: Order, completion: @escaping (CreateOrderResponse?) -> ()) {
+        guard let url = URL(string: "https://even-roasted-bike.glitch.me/orders") else {
+            fatalError("Invalid Url")
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try? JSONEncoder().encode(order)
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let data = data, error == nil else {
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+                return
+            }
+            
+           let createOrderResponse = try? JSONDecoder().decode(CreateOrderResponse.self, from: data)
+            
+            DispatchQueue.main.async {
+                completion(createOrderResponse)
+            }
+        }.resume()
+    }
+
     func getAllOrders(completion: @escaping ([Order]?) -> ()) {
         guard let url = URL(string: "https://even-roasted-bike.glitch.me/orders") else {
             DispatchQueue.main.async {
@@ -29,7 +54,6 @@ class CoffeeService {
             DispatchQueue.main.async {
                 completion(orders)
             }
-            
         }.resume()
         
     }
